92b7c57f415407cd9b8e33bc7e7c40d042efddca
princek <princesamuelpks@gmail>
2025-10-16 06:48:51 +0000

    Create testimonials-5.jpg

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ContactMessage, NewsletterSubscriber
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "subject", "created_at")
+    search_fields = ("name", "email", "subject", "message")
+    list_filter = ("created_at",)
+
+
+@admin.register(NewsletterSubscriber)
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("email", "subscribed_at")
+    search_fields = ("email",)
+    list_filter = ("subscribed_at",)
+from django.contrib import admin
 
 # Register your models here.
