953a4ce0babef1431eb968f0b340e8b485e8219d
princek <princesamuelpks@gmail>
2025-10-16 06:49:05 +0000

    Create urls.py

---
+++ website/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import HomeView, contact_submit, newsletter_subscribe
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('contact/', contact_submit, name='contact'),
+    path('newsletter/', newsletter_subscribe, name='newsletter'),
+]
+from django.urls import path
+from website.views import HomeView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('contact/', contact_submit, name='contact'),
+    path('newsletter/', newsletter_subscribe, name='newsletter'),
+]
