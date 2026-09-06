1c83e9bd154dd147df154cb7b12c83bb00a954ad
princek <princesamuelpks@gmail.com>
2026-09-06 01:16:28 +0000

    Load production environment deterministically

--- afrilearnsite/settings.py
+++ afrilearnsite/settings.py
@@ -4,10 +4,9 @@ import os
 from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 def env_bool(name, default=False):
